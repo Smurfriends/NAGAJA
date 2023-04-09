2bbb2a5162825c595e3454050d7f4e41ee80f3e1
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2023-04-09 20:20:09 +0900

    opencv

---
+++ sdk1/build/intermediates/cxx/Debug/55s424r1/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\tstne\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\NAGAJA\\sdk1\\.cxx\\Debug\\55s424r1\\armeabi-v7a" ^
+  opencv_jni_shared
---
+++ sdk1/build/intermediates/cxx/Debug/55s424r1/logs/x86/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\tstne\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\NAGAJA\\sdk1\\.cxx\\Debug\\55s424r1\\x86" ^
+  opencv_jni_shared
