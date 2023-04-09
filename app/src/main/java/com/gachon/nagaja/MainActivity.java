2392ec867697661704026d8a0aa234e22e387da5
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2023-04-09 19:51:48 +0900

    opemcv Setting

--- app/src/main/java/com/gachon/nagaja/MainActivity.java
+++ app/src/main/java/com/gachon/nagaja/MainActivity.java
@@ -3,6 +3,23 @@ package com.gachon.nagaja;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import org.opencv.android.BaseLoaderCallback;
+
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+
+import org.opencv.android.LoaderCallbackInterface;
+
+import org.opencv.android.OpenCVLoader;
+
+import org.opencv.core.Mat;
+
+import org.opencv.android.CameraBridgeViewBase;
+
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+
+import android.view.SurfaceView;
+
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
