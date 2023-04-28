bfb7cad4308e5700c70ae63d73109ac4eae6bbc5
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2023-04-28 10:27:20 +0900

    try make detect Line
    Make Gray finction and Line detect

--- app/src/main/java/com/gachon/nagaja/MainActivity.java
+++ app/src/main/java/com/gachon/nagaja/MainActivity.java
@@ -3,50 +3,29 @@ package com.gachon.nagaja;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
-import org.opencv.android.BaseLoaderCallback;
-
-import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
-
-import org.opencv.android.LoaderCallbackInterface;
-
 import org.opencv.android.OpenCVLoader;
 
 import org.opencv.core.DMatch;
 import org.opencv.core.Mat;
 
-import org.opencv.android.CameraBridgeViewBase;
-
-import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
-
-import android.provider.MediaStore;
-import android.view.SurfaceView;
-
-import android.view.WindowManager;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.os.Bundle;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.CvType;
-import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
-import java.io.File;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -54,22 +33,16 @@ import java.util.List;
 import androidx.annotation.Nullable;
 import org.opencv.core.Core;
 import org.opencv.core.MatOfInt4;
-import org.opencv.core.MatOfInt4;
 import org.opencv.core.Point;
-//import org.opencv.imgproc.HoughLinesP;
 import org.opencv.core.Scalar;
-import org.opencv.core.Mat;
 import org.opencv.core.MatOfDMatch;
 import org.opencv.core.MatOfKeyPoint;
-import org.opencv.core.Scalar;
 import org.opencv.features2d.AKAZE;
 import org.opencv.features2d.DescriptorMatcher;
 import org.opencv.features2d.Features2d;
-import org.opencv.imgproc.Imgproc;
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_CODE_PICK_IMAGE = 1;
-    private static final int REQUEST_CODE_READ_EXTERNAL_STORAGE_PERMISSION = 2;
 
     ImageView imageView;
     Button button;
@@ -107,9 +80,7 @@ public class MainActivity extends AppCompatActivity {
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                convertImageToGrayscale();
-            }
+            public void onClick(View v) { mainFunction(imageView); }
         });
 
         selectImageButton.setOnClickListener(new View.OnClickListener() {
@@ -127,11 +98,22 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    private void mainFunction(ImageView image){
+        // Convert the image to gray
+        Mat grayMat = convertImageToGrayscale(image);
 
-    private void convertImageToGrayscale() {
+        // Perform line detection
+        Mat lineMat = detectLines(grayMat);
+
+        // Set the resulting line detection image to the ImageView
+        Bitmap lineBitmap = Bitmap.createBitmap(lineMat.cols(), lineMat.rows(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(lineMat, lineBitmap);
+        image.setImageBitmap(lineBitmap);
+    }
+
+    // Make Image to gray (ImageView -> Mat)
+    private Mat convertImageToGrayscale(ImageView imageView) {
         try {
-            // Get the selected image from the ImageView
-            ImageView imageView = findViewById(R.id.imageView);
             Bitmap imageBitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
 
             // Convert the image to grayscale using OpenCV
@@ -141,51 +123,14 @@ public class MainActivity extends AppCompatActivity {
             Mat grayMat = new Mat(rgbaMat.size(), CvType.CV_8UC1);
             Imgproc.cvtColor(rgbaMat, grayMat, Imgproc.COLOR_RGBA2GRAY);
 
-            Mat lineMat = detectLines(rgbaMat, grayMat);
-
-            Bitmap resultBitmap = Bitmap.createBitmap(rgbaMat.cols(), rgbaMat.rows(), Bitmap.Config.ARGB_8888);
-            Utils.matToBitmap(lineMat, resultBitmap);
-            imageView.setImageBitmap(resultBitmap);
-
-            Bitmap grayBitmap = Bitmap.createBitmap(grayMat.cols(), grayMat.rows(), Bitmap.Config.ARGB_8888);
-            Utils.matToBitmap(grayMat, grayBitmap);
-
-            imageView.setImageBitmap(grayBitmap);
+            return grayMat;
         } catch (Exception e) {
             Log.e("OpenCV", "Error processing image", e);
+            return null;
         }
     }
-
-//    private void convertImageToGrayscale() {
-//        try {
-//            // Load the image from the drawable folder
-//            Bitmap imageBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.data1);
-//
-//            // Convert the image to grayscale using OpenCV
-//            Mat rgbaMat = new Mat();
-//            Utils.bitmapToMat(imageBitmap, rgbaMat);
-//
-//            Mat grayMat = new Mat(rgbaMat.size(), CvType.CV_8UC1);
-//            Imgproc.cvtColor(rgbaMat, grayMat, Imgproc.COLOR_RGBA2GRAY);
-//
-//            Mat lineMat = detectLines(rgbaMat, grayMat);
-//
-//            Bitmap resultBitmap = Bitmap.createBitmap(rgbaMat.cols(), rgbaMat.rows(), Bitmap.Config.ARGB_8888);
-//            Utils.matToBitmap(lineMat, resultBitmap);
-//            imageView.setImageBitmap(resultBitmap);
-//
-//            Bitmap grayBitmap = Bitmap.createBitmap(grayMat.cols(), grayMat.rows(), Bitmap.Config.ARGB_8888);
-//            Utils.matToBitmap(grayMat, grayBitmap);
-//
-//
-//            imageView.setImageBitmap(grayBitmap);
-//        } catch (Exception e) {
-//            Log.e("OpenCV", "Error processing image", e);
-//        }
-//    }
-
-
-    private Mat detectLines(Mat mapMat, Mat grayMat){
+    // Detect lines in the grayscale image
+    private Mat detectLines(Mat grayMat){
         Mat edgesMat = new Mat();
 
         Imgproc.Canny(grayMat, edgesMat, 100, 200);
@@ -204,31 +149,27 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void selectImageFromGallery() {
+    private void selectImageFromGallery() {// Select Image
         Intent pickImageIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         pickImageIntent.addCategory(Intent.CATEGORY_OPENABLE);
         pickImageIntent.setType("image/*");
         startActivityForResult(pickImageIntent, REQUEST_CODE_PICK_IMAGE);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE_PICK_IMAGE && resultCode == RESULT_OK && data != null) {
-            Uri selectedImageUri = data.getData();
-            if (selectedImageUri != null) {
-                try {
-                    InputStream inputStream = getContentResolver().openInputStream(selectedImageUri);
-                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-                    imageView.setImageBitmap(bitmap);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
+    public void setGrayscaleImage(Mat rgbaMat, ImageView imageView) {
+        try {
+            // Convert the image to grayscale using OpenCV
+            Mat grayMat = new Mat();
+            Imgproc.cvtColor(rgbaMat, grayMat, Imgproc.COLOR_RGBA2GRAY);
+
+            Bitmap grayBitmap = Bitmap.createBitmap(grayMat.cols(), grayMat.rows(), Bitmap.Config.ARGB_8888);
+            Utils.matToBitmap(grayMat, grayBitmap);
+
+            imageView.setImageBitmap(grayBitmap);
+        } catch (Exception e) {
+            Log.e("OpenCV", "Error processing image", e);
         }
     }
-
-
     public Mat findSubImage(Mat fullImage, Mat subImage, boolean useRGB, double accuracyThreshold) {
         // Convert images to grayscale if required
         if (!useRGB) {
@@ -280,5 +221,41 @@ public class MainActivity extends AppCompatActivity {
         // Set the Bitmap as the content of the ImageView
         ImageView imageView = findViewById(R.id.imageView);
         imageView.setImageBitmap(outputBitmap);
+    }
+
+
+    public static void detectExitSign(String imagePath) {
+        Mat src = Imgcodecs.imread(imagePath);
+
+        if (src.empty()) {
+            System.err.println("Image not found!");
+            return;
+        }
+
+        Mat gray = new Mat();
+        Imgproc.cvtColor(src, gray, Imgproc.COLOR_BGR2GRAY);
+
+        Mat edges = new Mat();
+        Imgproc.Canny(gray, edges, 50, 150);
+
+        List<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchy = new Mat();
+        Imgproc.findContours(edges, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        Mat result = src.clone();
+        for (int i = 0; i < contours.size(); i++) {
+            Rect rect = Imgproc.boundingRect(contours.get(i));
+            if (isExitSign(rect)) {
+                Imgproc.rectangle(result, rect.tl(), rect.br(), new Scalar(0, 255, 0), 2);
+            }
+        }
+
+        Imgcodecs.imwrite("path/to/output.jpg", result);
+        System.out.println("Exit sign detection complete.");
+    }
+
+    private static boolean isExitSign(Rect rect) {
+        double aspectRatio = (double) rect.width / rect.height;
+        return aspectRatio >= 1.0 && aspectRatio <= 2.0;
     }
 }
