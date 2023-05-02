268a88abc6357d099d37f642312802c38edc69af
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2023-05-02 10:08:38 +0900

    Camera OK

---
+++ app/src/main/java/com/gachon/nagaja/LoadingActivity.java
@@ -0,0 +1,27 @@
+package com.gachon.nagaja;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoadingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loading);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LoadingActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
+    }
+}
