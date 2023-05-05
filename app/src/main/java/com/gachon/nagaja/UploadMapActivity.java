28cf33d5cca76c6c3746379baef33849eba1aec8
zincyan <donggak0617@naver.com>
2023-05-06 00:01:33 +0900

    Move the code for loading by Jihae to UploadMapActivity

--- app/src/main/java/com/gachon/nagaja/UploadMapActivity.java
+++ app/src/main/java/com/gachon/nagaja/UploadMapActivity.java
@@ -2,6 +2,9 @@ package com.gachon.nagaja;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,7 +32,8 @@ public class UploadMapActivity extends AppCompatActivity {
         floorUpgroundNum = findViewById(R.id.floorUpgroundNum);
         floorOutsideEntrance = findViewById(R.id.floorOutsideEntrance);
 
-        uploadImgBtn = findViewById(R.id.uploadImgBtn);
+        cameraBtn = findViewById(R.id.cameraBtn);
+        selectImgBtn = findViewById(R.id.selectImgBtn);
         imageView = findViewById(R.id.imageView);
         floorOfMap = findViewById(R.id.floorOfMap);
         submitBtn = findViewById(R.id.submitBtn);
@@ -51,16 +55,70 @@ public class UploadMapActivity extends AppCompatActivity {
             }
         });
 
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pictureImageByCamera();
+            }
+        });
+
+        selectImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImageFromGallery();
+            }
+        });
+
+    }
+
+    private void pictureImageByCamera(){
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, TAKE_PICTURE);
+    }
+
+    private void selectImageFromGallery() {// Select Image
+        Intent pickImageIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        pickImageIntent.addCategory(Intent.CATEGORY_OPENABLE);
+        pickImageIntent.setType("image/*");
+        startActivityForResult(pickImageIntent, REQUEST_CODE_PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case TAKE_PICTURE:
+                if (resultCode == RESULT_OK && data.hasExtra("data")) {
+                    Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                    if (bitmap != null) {
+                        imageView.setImageBitmap(bitmap);
+                    }
+
+                }
+                break;
+            case REQUEST_CODE_PICK_IMAGE:
+                if (resultCode == RESULT_OK && data != null) {
+                    Uri uri = data.getData();
+                    // Set the image in ImageView
+                    imageView.setImageURI(uri);
+
+                }
+                break;
+
+        }
     }
 
 
     // declaration
+    private static final int REQUEST_CODE_PICK_IMAGE = 1;
+    private static final int TAKE_PICTURE = 2;
     private EditText address;
     private EditText buildingName;
     private EditText floorUndergroundNum;
     private EditText floorUpgroundNum;
     private EditText floorOutsideEntrance;
-    private Button uploadImgBtn;
+    private Button cameraBtn;
+    private Button selectImgBtn;
     private ImageView imageView;
     private EditText floorOfMap;
     private Button submitBtn;
