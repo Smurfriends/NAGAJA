a42e68bc24836fff053b7fe7b81bd8acf1e6847c
zincyan <donggak0617@naver.com>
2023-05-06 15:14:43 +0900

    Create MapDTO and Insert imageURL data into database (URL of firebase storage)

---
+++ app/src/main/java/com/gachon/nagaja/MapDTO.java
@@ -0,0 +1,22 @@
+package com.gachon.nagaja;
+
+public class MapDTO {
+
+    public String mapURL;
+
+    MapDTO() {
+    }
+
+    public MapDTO(String mapURL) {
+        this.mapURL = mapURL;
+    }
+
+//    public void setMapURL(String mapURL) {
+//        this.mapURL = mapURL;
+//    }
+
+    public String getMapURL(String mapURL) {
+        return mapURL;
+    }
+
+}
--- app/src/main/java/com/gachon/nagaja/UploadMapActivity.java
+++ app/src/main/java/com/gachon/nagaja/UploadMapActivity.java
@@ -1,25 +1,38 @@
 package com.gachon.nagaja;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class UploadMapActivity extends AppCompatActivity {
 
 //    private FirebaseAuth mAuth;   // 권한 추가 예정
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference databaseReference = firebaseDatabase.getReference();
+    //private final DatabaseReference root = FirebaseDatabase.getInstance().getReference("Image");
+    private final StorageReference reference = FirebaseStorage.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,12 +57,23 @@ public class UploadMapActivity extends AppCompatActivity {
                 if (address.getText().toString().equals(""))
                     return;
 
+                // upload building data
                 String undergroundNum = floorUndergroundNum.getText().toString();
                 String upgroundNum = floorUpgroundNum.getText().toString();
 
                 BuildingDTO building = new BuildingDTO(buildingName.getText().toString(),
                         Integer.parseInt(undergroundNum), Integer.parseInt(upgroundNum));
                 databaseReference.child("building").child(address.getText().toString()).setValue(building);
+
+                // upload map data
+                if (imageUri != null) { // 파일에서 선택했다면
+                    uploadToFirebase(imageUri);
+                }
+                else {
+                    // 카메라로 찍어서 올린 map이미지 업로드는 아직 구현 못함
+                    Toast.makeText(UploadMapActivity.this, "사진을 선택해주세요.", Toast.LENGTH_SHORT).show();
+                }
+
                 // TODO: then back to main
 
             }
@@ -92,21 +116,59 @@ public class UploadMapActivity extends AppCompatActivity {
                     Bitmap bitmap = (Bitmap) data.getExtras().get("data");
                     if (bitmap != null) {
                         imageView.setImageBitmap(bitmap);
+                        imageUri = null;    // initialize imageUri
                     }
 
                 }
                 break;
             case REQUEST_CODE_PICK_IMAGE:
                 if (resultCode == RESULT_OK && data != null) {
-                    Uri uri = data.getData();
+                    imageUri = data.getData();
                     // Set the image in ImageView
-                    imageView.setImageURI(uri);
+                    imageView.setImageURI(imageUri);
 
                 }
                 break;
 
         }
     }
+
+    //파이어베이스 이미지 업로드
+    private void uploadToFirebase(Uri imgUri) {
+
+        StorageReference fileRef = reference.child(System.currentTimeMillis() + "." + getFileExtension(imgUri));
+
+        fileRef.putFile(imgUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+
+                        //이미지 모델에 담기
+                        MapDTO newMap = new MapDTO(uri.toString());
+
+                        //데이터 넣기
+                        databaseReference.child("map").child(address.getText().toString()).setValue(newMap);
+                        // 층별 구분 넣기 필요
+                        Toast.makeText(UploadMapActivity.this, "업로드 성공", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+    }
+
+    //파일타입 가져오기
+    private String getFileExtension(Uri uri) {
+
+        ContentResolver cr = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+
+        return mime.getExtensionFromMimeType(cr.getType(uri));
+    }
+
 
 
     // declaration
@@ -122,5 +184,6 @@ public class UploadMapActivity extends AppCompatActivity {
     private ImageView imageView;
     private EditText floorOfMap;
     private Button submitBtn;
+    private Uri imageUri;
 
 }
