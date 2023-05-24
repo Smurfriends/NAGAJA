ef508f8858293846dcac64a6ab986f7b62723fd4
Jihae <62400658+easyssun@users.noreply.github.com>
2023-05-24 19:34:35 +0900

    gradle change
    7.4.0-alpha08

---
+++ sdk1/build/intermediates/cxx/Debug/371x1116/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\82102\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\NAGAJA\\sdk1\\.cxx\\Debug\\371x1116\\arm64-v8a" ^
+  opencv_jni_shared
---
+++ sdk1/build/intermediates/cxx/Debug/371x1116/logs/arm64-v8a/configure_command.bat
@@ -0,0 +1,19 @@
+@echo off
+"C:\\Users\\82102\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\cmake.exe" ^
+  "-HC:\\NAGAJA\\sdk1\\libcxx_helper" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=arm64-v8a" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=arm64-v8a" ^
+  "-DANDROID_NDK=C:\\Users\\82102\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\82102\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\82102\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\82102\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\NAGAJA\\sdk1\\build\\intermediates\\cxx\\Debug\\371x1116\\obj\\arm64-v8a" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\NAGAJA\\sdk1\\build\\intermediates\\cxx\\Debug\\371x1116\\obj\\arm64-v8a" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BC:\\NAGAJA\\sdk1\\.cxx\\Debug\\371x1116\\arm64-v8a" ^
+  -GNinja ^
+  "-DANDROID_STL=c++_shared"
