6ba3d5f710f51bbdd3bbe3b72ef2b651fa5fffdf
zincyan <donggak0617@naver.com>
2023-05-31 17:45:03 +0900

    isInRange update

--- sdk1/build/intermediates/cxx/Debug/55s424r1/logs/arm64-v8a/configure_command.bat
+++ sdk1/build/intermediates/cxx/Debug/55s424r1/logs/arm64-v8a/configure_command.bat
@@ -1,19 +1,19 @@
 @echo off
-"C:\\Users\\tstne\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
-  "-HC:\\NAGAJA\\sdk1\\libcxx_helper" ^
+"C:\\Users\\jiwon kim\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\jiwon kim\\Documents\\GitHub\\NAGAJA\\sdk1\\libcxx_helper" ^
   "-DCMAKE_SYSTEM_NAME=Android" ^
   "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
   "-DCMAKE_SYSTEM_VERSION=21" ^
   "-DANDROID_PLATFORM=android-21" ^
   "-DANDROID_ABI=arm64-v8a" ^
   "-DCMAKE_ANDROID_ARCH_ABI=arm64-v8a" ^
-  "-DANDROID_NDK=C:\\Users\\tstne\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620" ^
-  "-DCMAKE_ANDROID_NDK=C:\\Users\\tstne\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620" ^
-  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\tstne\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620\\build\\cmake\\android.toolchain.cmake" ^
-  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\tstne\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\NAGAJA\\sdk1\\build\\intermediates\\cxx\\Debug\\55s424r1\\obj\\arm64-v8a" ^
-  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\NAGAJA\\sdk1\\build\\intermediates\\cxx\\Debug\\55s424r1\\obj\\arm64-v8a" ^
+  "-DANDROID_NDK=C:\\Users\\jiwon kim\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\jiwon kim\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\jiwon kim\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\jiwon kim\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\jiwon kim\\Documents\\GitHub\\NAGAJA\\sdk1\\build\\intermediates\\cxx\\Debug\\55s424r1\\obj\\arm64-v8a" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\jiwon kim\\Documents\\GitHub\\NAGAJA\\sdk1\\build\\intermediates\\cxx\\Debug\\55s424r1\\obj\\arm64-v8a" ^
   "-DCMAKE_BUILD_TYPE=Debug" ^
-  "-BC:\\NAGAJA\\sdk1\\.cxx\\Debug\\55s424r1\\arm64-v8a" ^
+  "-BC:\\Users\\jiwon kim\\Documents\\GitHub\\NAGAJA\\sdk1\\.cxx\\Debug\\55s424r1\\arm64-v8a" ^
   -GNinja ^
   "-DANDROID_STL=c++_shared"
